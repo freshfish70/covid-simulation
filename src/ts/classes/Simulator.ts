d2f45947f15407db5c2b97b5dee4a61a55241f81
Christoffer A Træen <christoffer.andersen@live.com>
2020-04-11 16:32:50 +0200

    Proof of concept scenarios

--- src/ts/classes/Simulator.ts
+++ src/ts/classes/Simulator.ts
@@ -3,6 +3,7 @@ import { Grid } from './Grid'
 import { Person, Compartment } from './Person'
 import { Point } from './Point'
 import { QuadTree } from './QuadTree'
+import { Obstacle } from './Obstacle'
 
 export class Simulator {
 	private _p5: p5
@@ -29,6 +30,10 @@ export class Simulator {
 	// All persons that are in the simulations
 	private _persons: Array<Person> = new Array()
 
+	private _obstacles: Array<Obstacle> = new Array()
+
+	private peakInfectedConcurrent = 0
+
 	// ! Temporary
 
 	private infected = 0
@@ -38,6 +43,7 @@ export class Simulator {
 	private reocvered = 0
 
 	private dead = 0
+	private infectedPeople = 0
 
 	// !END TEMPORARY
 
@@ -61,6 +67,14 @@ export class Simulator {
 		this.initialize()
 	}
 
+	public start() {
+		this._p5.loop()
+	}
+
+	public pause() {
+		this._p5.noLoop()
+	}
+
 	/**
 	 * Initializes P5 and bind the setup and
 	 * draw functions
@@ -71,10 +85,13 @@ export class Simulator {
 			 * Runs once
 			 */
 			sketch.setup = () => {
-				sketch.createCanvas(this._width, this._height)
+				sketch.createCanvas(this._width, this._height, 'p2d')
 				this.populateGrid()
 				sketch.frameRate(this._framerate)
+				sketch.background('#081B2E')
 				console.log('Setup complete')
+
+				// sketch.noLoop()
 			}
 			/**
 			 * Runs each frame
@@ -86,6 +103,7 @@ export class Simulator {
 					this.infected = 0
 					this.reocvered = 0
 					this.dead = 0
+					this.infectedPeople = 0
 
 					sketch.background('#081B2E')
 					// this.showGrid()
@@ -110,6 +128,9 @@ export class Simulator {
 								sketch.stroke('#FFDE91')
 								break
 							case Compartment.INFECTED:
+								this.infectedPeople +=
+									person.peopleInfected + person.avgInfections
+
 								this.infected++
 								sketch.stroke('#B32144')
 								// sketch.stroke('#4265B3')
@@ -135,8 +156,32 @@ export class Simulator {
 						)
 						person?.act()
 					}
+					for (const obs of this._obstacles) {
+						sketch.stroke('#fff')
+
+						sketch.strokeWeight(1)
+						sketch.rectMode('center')
+						sketch.rect(
+							obs.position.x * this._cellSize + 5,
+							obs.position.y * this._cellSize + 5,
+							2,
+							2
+						)
+					}
+
+					// console.log(this.infectedPeople)
+					if (this.infected > this.peakInfectedConcurrent) {
+						this.peakInfectedConcurrent = this.infected
+					}
+					// this.infectedPeople = (this.infected / this.infectedPeople).toFixed(3)
+					this.infectedPeople = (this.infectedPeople / this.infected).toFixed(3)
 				}
 				this.showStats(sketch)
+				if (this.infected == 0 && this.reocvered > 0) {
+					console.log('DONE')
+					console.log('Peak :' + this.peakInfectedConcurrent)
+					this.pause()
+				}
 			}
 		}
 		this._p5 = new p5(runable)
@@ -157,9 +202,14 @@ export class Simulator {
 
 		let basePos = 80
 		sketch.text('SU: ' + this.suceptible, basePos, this._height - 10)
-		sketch.text('IN: ' + this.infected, basePos + 50, this._height - 10)
-		sketch.text('RE: ' + this.reocvered, basePos + 50 * 2, this._height - 10)
-		sketch.text('DE: ' + this.dead, basePos + 50 * 3, this._height - 10)
+		sketch.text('IN: ' + this.infected, basePos + 100, this._height - 10)
+		sketch.text('RE: ' + this.reocvered, basePos + 100 * 2, this._height - 10)
+		sketch.text('DE: ' + this.dead, basePos + 100 * 3, this._height - 10)
+		sketch.text(
+			'R: ' + this.infectedPeople,
+			basePos + 50 * 4,
+			this._height - 10
+		)
 	}
 
 	/**
@@ -177,6 +227,12 @@ export class Simulator {
 		}
 	}
 
+	private forcedQuarantine = false
+
+	private quarterMoving = false
+
+	private oneInEightMoving = true
+
 	/**
 	 * Populates the grid with people at random positions
 	 */
@@ -186,13 +242,33 @@ export class Simulator {
 		// Used for debugging tries the selection of cells uses, and also for
 		// failsafing the while loop :D
 		let tries = 0
-		while (chosenLocations < this._entities && tries < 10000) {
+		console.log(this._entities)
+
+		let qpopulationQuarantine = this._entities - this._entities / 4
+		console.log(qpopulationQuarantine)
+
+		let epopulationQuarantine = this._entities - this._entities / 8
+
+		let quarantined = 0
+
+		if (this.forcedQuarantine) this.forcedQurantine()
+
+		while (chosenLocations < this._entities && tries < 100000) {
 			let chosenX = Math.floor(Math.random() * this._simulationArea.sizeX)
 			let chosenY = Math.floor(Math.random() * this._simulationArea.sizeY)
 			let location = new Point(chosenX, chosenY)
 
 			if (this._simulationArea.getObjectAtLocation(location) == null) {
 				let p = new Person(this._simulationArea, location)
+				if (this.quarterMoving && quarantined <= qpopulationQuarantine) {
+					p.isInQuarantine = true
+					quarantined++
+				}
+				if (this.oneInEightMoving && quarantined <= epopulationQuarantine) {
+					p.isInQuarantine = true
+					quarantined++
+				}
+
 				this._persons.push(p)
 				this._simulationArea.addToLocation(p, location)
 				chosenLocations++
@@ -201,8 +277,45 @@ export class Simulator {
 		}
 
 		//! INFECT ONLY A SINGLE PERSON
-		this._persons[0].infect()
+		this._persons[90].infect()
 
 		console.log(`Generated ${this._entities} in ${tries} tries`)
+	}
+
+	private forcedQurantine() {
+		const cellsHeight = this._height / this._cellSize
+		for (let index = 0; index < cellsHeight; index++) {
+			let po = new Point(15, index)
+			let ob = new Obstacle(this._simulationArea, po)
+			this._simulationArea.addToLocation(ob, po)
+			this._obstacles.push(ob)
+		}
+		this.timer()
+	}
+
+	private timer() {
+		let times = 0
+		const me = setInterval(
+			() => {
+				const min = Math.floor(this._obstacles.length / 2) - times
+				const max = Math.floor(this._obstacles.length / 2) + times
+
+				for (let index = min; index < max; index++) {
+					const element = this._obstacles[index]
+					if (!element) continue
+					this._simulationArea.freeLocation(element.position)
+				}
+
+				if (times < 3) {
+					times++
+				} else {
+					console.log('Cleared interval')
+					clearInterval(me)
+				}
+				this._obstacles.splice(min, max - min)
+			},
+			2000,
+			this
+		)
 	}
 }
